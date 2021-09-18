aded0d80e185ba718be46eeeecef3a4b0c256bb8
oblerion <82583559+oblerion@users.noreply.github.com>
2021-09-18 15:11:41 +0200

    first post

---
+++ make_all_love.bash
@@ -0,0 +1,22 @@
+#! /bin/bash
+# love2d 32bit export script for linux 
+# make by desnot4000
+loveexe_path="/home/desnot/GD/love-11.3-win32/LOVE2D";
+c=2
+for n in *.love ; do
+	love_name=${n:0:${#n}-5}
+	cat ${loveexe_path}/love.exe $n > ${love_name}.exe
+	echo ">> $n to ${love_name}.exe ok"
+	if [ ! -d "${love_name}" ];then mkdir ${love_name} ;fi
+	if [ ! -f "${love_name}/love.dll" ];then cp ${loveexe_path}/*.dll ${love_name} ;fi 
+	if [ -f "${love_name}.exe" ];then mv ${love_name}.exe ${love_name} ;fi
+	while [[ c!=1 && c!=0 ]] ;do
+		echo "do you want zip game? 1/yes 0/no "
+		read c
+		if [ c==1 ];then
+			if [ ! -f "${n:5:${#n}-10}.zip" ];then zip -r ${love_name}.zip ${love_name};echo ">> build ${love_name}.zip ok" ;fi	
+		else
+			break;
+		fi
+	done
+done
